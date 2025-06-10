d2b55927e0c56a5a71837ee5b4cd64b7abb36ad9
manandevs <abdulmanan.devs@gmail.com>
2025-06-10 12:19:44 -0700

    Describe your changes here

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        {/* fallback for older browsers */}
+        <link rel="alternate icon" type="image/png" href="/favicon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
